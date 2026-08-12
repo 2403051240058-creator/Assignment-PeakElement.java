ad8993bb055f62c5af6fe0d793d63a7d56200cb4
Palak Gupta <2403051240058@paruluniversity.ac.in>
2026-08-12 22:41:57 +0530

    Create .java

---
+++ .java
@@ -0,0 +1,13 @@
+//question 35//
+class Solution:
+    def searchInsert(self, nums, target):
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right)
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left
